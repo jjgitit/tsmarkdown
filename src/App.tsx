3478108ef095fe7872aac9a1abd8b9525413cbd3
jjgitit <jaeyeonee0320@gmail.com>
2025-01-06 20:13:46 -0800

    getting notes from local storage with tags

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Container } from "react-bootstrap";
 import { Route, Routes, Navigate } from "react-router-dom";
 import { NewNote } from "./NewNote";
 import { useLocalStorage } from "./useLocalStorage";
+import { useMemo } from "react";
 
 export type Note = {
   id: string;
@@ -11,7 +12,7 @@ export type Note = {
 
 export type RawNote = {
   id: string;
-};
+} & RawNoteData;
 
 export type RawNoteData = {
   title: string;
@@ -33,6 +34,15 @@ export type Tag = {
 function App() {
   const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
   const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+
+  const notesWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
   return (
     <Container className="my-4">
       <Routes>
